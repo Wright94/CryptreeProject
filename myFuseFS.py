f13e8251604b51602acdea606564b9f48a9b841f
Harry Wright <harrywright94@hotmail.co.uk>
2016-12-08 19:12:02 +0000

    Defined filesystem methods and the __init__ method

--- myFuseFS.py
+++ myFuseFS.py
@@ -9,3 +9,77 @@ import errno
 sys.path.append("/Users/Harry/Library/Python/3.5/lib/python/site-packages")
 
 from fuse import FUSE, FuseOSError, Operations
+
+
+# This script created a pass-through file system. It simply accepts one directory and simulates it under the mountpoint,
+# ensuring that all changes made in the mountpoint are mirrored in the source directory.
+#-----------------------------------------------------------------------------------------------------------------------
+
+class MyFilesystem(Operations):
+
+    def __init__(self, root):
+        self.root = root
+
+    def _full_path(self, partial):
+        if partial.startswith("/"):
+            partial = partial[1:]
+        path = os.path.join(self.root, partial)
+        return path
+
+    # Below, the Filesystem methods are defined.
+    #-------------------------------------------
+
+    def readdir(self, path, fh):
+        full_path = self._full_path(path)
+
+        dirents = ['.', '..']
+        if os.path.isdir(full_path):
+            dirents.extend(os.listdir(full_path))
+        for r in dirents:
+            yield r
+
+    def readlink(self, path):
+        pathname = os.readlink(self._full_path(path))
+        if pathname.startswith("/"):
+            # Path name is absolute, sanitize it.
+            return os.path.relpath(pathname, self.root)
+        else:
+            return pathname
+
+    def mknod(self, path, mode, dev):
+        return os.mknod(self._full_path(path), mode, dev)
+
+    def rmdir(self, path):
+        full_path = self._full_path(path)
+        return os.rmdir(full_path)
+
+    def mkdir(self, path, mode):
+        return os.mkdir(self._full_path(path), mode)
+
+    def access(self, path, mode):
+        full_path = self._full_path(path)
+        if not os.access(full_path, mode):
+            raise FuseOSError(errno.EACCES)
+
+    def chmod(self, path, mode):
+        full_path = self._full_path(path)
+        return os.chmod(full_path, mode)
+
+    def chown(self, path, uid, gid):
+        full_path = self._full_path(path)
+        return os.chown(full_path, uid, gid)
+
+    def getattr(self, path, fh=None):
+        full_path = self._full_path(path)
+        st = os.lstat(full_path)
+        return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
+                     'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
+
+
+    # Main
+    # ----
+def main(mountpoint, root):
+    FUSE(MyFilesystem(root), mountpoint, nothreads=True, foreground=True)
+
+if __name__ == '__main__':
+    main(sys.argv[2], sys.argv[1])
